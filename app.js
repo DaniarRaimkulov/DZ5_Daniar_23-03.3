c93ed52f5db0933dc1cc2bf30f4c10e97131945f
”Raimkulov <daniarraimkulov123@gmail.com>
2023-03-29 14:50:01 +0600

    push

--- app.js
+++ app.js
@@ -1,69 +1,42 @@
 const som = document.querySelector('#som')
 const usd = document.querySelector('#usd')
 const eur = document.querySelector('#eur')
-
-const convert = (elem, target, rate) => {
-    elem.oninput = () => {
-        const request = new XMLHttpRequest()
-        request.open("GET", "data.json")
-        request.setRequestHeader("Content-type", "application/json")
-        request.send()
+const requestData = () => {
+    const request = new XMLHttpRequest()
+    request.open("GET", "data.json")
+    request.setRequestHeader("Content-type", "application/json")
+    request.send()
+    return new Promise((resolve, reject) => {
         request.onload = () => {
             const response = JSON.parse(request.responseText)
+            resolve(response)
+        }
+        request.onerror = () => reject(request.statusText)
+    })
+}
+const convert = async (elem, target, rate) => {
+    elem.oninput = async () => {
+        try {
+            const response = await requestData()
             if (elem.value === '') {
                 target.value = ''
             } else {
                 target.value = (elem.value * response[rate] / response.usd).toFixed(2)
             }
+        } catch (error) {
+            console.error(error)
         }
     }
 }
+
 convert(eur, som, 'eur')
 convert(som, usd, 'usd')
 convert(som, eur, 'eur')
 convert(usd, som, 'usd')
 
-usd.oninput = () => {
-    const request = new XMLHttpRequest()
-    request.open("GET", "data.json")
-    request.setRequestHeader("Content-type", "application/json")
-    request.send()
-    request.onload = () => {
-        const response = JSON.parse(request.responseText)
-        if (usd.value === '') {
-            som.value = ''
-            eur.value = ''
-        } else {
-            som.value = (usd.value * response.usd).toFixed(2)
-            eur.value = (som.value / response.eur).toFixed(2)
-        }
-    }
-}
-
-eur.oninput = () => {
-    const request = new XMLHttpRequest()
-    request.open("GET", "data.json")
-    request.setRequestHeader("Content-type", "application/json")
-    request.send()
-    request.onload = () => {
-        const response = JSON.parse(request.responseText)
-        if (eur.value === '') {
-            som.value = ''
-            usd.value = ''
-        } else {
-            som.value = (eur.value * response.eur).toFixed(2)
-            usd.value = (som.value / response.usd).toFixed(2)
-        }
-    }
-}
-
-som.oninput = () => {
-    const request = new XMLHttpRequest()
-    request.open("GET", "data.json")
-    request.setRequestHeader("Content-type", "application/json")
-    request.send()
-    request.onload = () => {
-        const response = JSON.parse(request.responseText)
+som.oninput = async () => {
+    try {
+        const response = await requestData()
         if (som.value === '') {
             usd.value = ''
             eur.value = ''
@@ -71,10 +44,7 @@ som.oninput = () => {
             usd.value = (som.value / response.usd).toFixed(2)
             eur.value = (som.value / response.eur).toFixed(2)
         }
+    } catch (error) {
+        console.error(error)
     }
 }
-
-
-
-
-
